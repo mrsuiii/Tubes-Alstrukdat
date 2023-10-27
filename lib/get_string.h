c0a06a3c68230011f5688cf9ee1d5bb15e00340a
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-27 10:04:03 +0700

    Implemented get_string

---
+++ lib/get_string.h
@@ -0,0 +1,6 @@
+#ifndef GET_STRING_H
+#define GET_STRING_H
+
+void get_string(char* res, int size);
+
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lib/greet.h"
+#include "lib/get_string.h"
 
 int main(){
-    greet("Bondowoso");
+    printf("Masukkan nama lengkap Anda: ");
+
+    int maxName = 100;
+    char name[maxName];
+    get_string(name, maxName);
+
+    greet(name);
 }

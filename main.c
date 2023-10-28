9dc34f0d345598ef63cd31e572c28c12081b5249
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-28 10:35:51 +0700

    Makefile for tmp file

--- main.c
+++ main.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
-#include "lib/routine.h"
-#include "lib/user.h"
-#include "lib/get_string.h"
-
 int main(){
-    setup();
-
-    UserId id = signUp();
-
-    displayUser(0);
-
-    cleanup();
 }

fd65aa352a125b8ded070c65205956c2c1be9f9f
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-11-03 21:26:07 +0700

    Config for read and write

--- lib/config.h
+++ lib/config.h
@@ -1,6 +1,16 @@
 #ifndef CONFIG
 #define CONFIG
 
+#define CONFIG_MARK '\n'
+#define MAX_CONFIG 1000000
+#define MAX_LINE 1000
+#define MAX_PATH 100
 
+void finishWriteFile();
+void startWriteFile(char* path);
+
+void startReadFile(char* path);
+void finishReadFile();
+void readNext(char* str, char mark, int max);
 
 #endif
